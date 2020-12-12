0959667bde9f6104545e6a36cad1dac6e719de13
Miklós Péch <miklos.pech@stud.uni-corvinus.hu>
2020-12-12 15:58:28 +0100

    commit4

---
+++ magicsq/magicsq/Magicsquare.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace magicsq
+{
+    class Magicsquare
+    {
+        public string Quiz { get; set; }
+        public string Solution { get; set; }
+    }
+}
